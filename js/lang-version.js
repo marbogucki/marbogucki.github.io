8e4a9fe57877e480bdda6c178b9aa5c3f24adde0
Marcin Bogucki <mar.bogucki@gmail.com>
2016-08-12 13:27:47 +0200

    update project

---
+++ js/lang-version.js
@@ -0,0 +1,23 @@
+var lang = {
+
+    pl: {
+
+        home: 'Strona Główna',
+        about: 'O mnie',
+        experience: 'Doświadczenie',
+        skills: 'Umiejętności',
+        contact: 'Kontakt'
+
+    },
+
+    en: {
+
+        home: 'Home',
+        about: 'About me',
+        experience: 'Experience',
+        skills: 'Skills',
+        contact: 'Contact'
+
+    }
+
+};
---
+++ js/script.js
@@ -0,0 +1,116 @@
+(function() {
+
+    'user strict';
+
+    $(document).ready(function() {
+
+        var singlePage = {
+            imagesArr: ['slide-01', 'slide-02', 'slide-03'],
+
+            $viewportHeight: $(window).height(),
+            $btnBackToTop: $('.btn-back-to-top'),
+
+            /* ----- INIT ----- */
+            init: function() {
+
+                singlePage.scrollHeaderMin();
+                singlePage.animateBannerImg();
+                singlePage.showHideNav();
+
+                $(window).on('scroll', function() {
+                    singlePage.scrollHeaderMin();
+                    singlePage.btnShowHide();
+                });
+
+                singlePage.btnBackToTop();
+                singlePage.btnShowHide();
+
+                singlePage.changeLanguageVersion();
+                singlePage.buttonChangeLanguage();
+            },
+
+            /* ----- show min header after scroll ----- */
+            scrollHeaderMin: function() {
+                var $siteHeader = $('.site-header');
+                var $bannerHeight = $('.site-banner').height();
+                var scrollPosition = $(window).scrollTop();
+                return (scrollPosition > $bannerHeight / 4) ? $siteHeader.addClass('header-min-scroll') : $siteHeader.removeClass('header-min-scroll');
+            },
+
+            /* ----- show and hide main navigation ----- */
+            showHideNav: function() {
+                $('.btn-mobile-nav').on('click', function(ev) {
+                    ev.preventDefault();
+                    var $button = $(this);
+                    $button.toggleClass('active');
+                    $('.nav-main').slideToggle();
+                });
+            },
+
+            /* ----- animate images / banner ----- */
+            animateBannerImg: function() {
+                var img = singlePage.imagesArr.shift();
+                singlePage.imagesArr.push(img);
+                $('.site-banner').css({
+                    'background-image': 'url("images/' + img + '.jpg")'
+                });
+                setTimeout(singlePage.animateBannerImg, 5000);
+            },
+
+
+            /* ----- language version ----- */
+            changeLanguageVersion: function() {
+                $('[data-lang]').each(function(index, el) {
+                    var $textLang = $(el).data('lang');
+                    $(el).text( lang [ localStorage.getItem('lang-version') || 'en'][ $textLang ] );
+                });
+
+                var currLang = localStorage.getItem('lang-version');
+                $('.nav-language li').removeClass('active');
+
+                if(currLang) {
+                    $('.nav-language a[data-ver-lang=' + currLang + ']').parent().addClass('active');
+                } else {
+                    $('.nav-language a[data-ver-lang=en]').parent().addClass('active');
+                }
+            },
+
+            /* ----- button change language ----- */
+            buttonChangeLanguage: function() {
+                $('.nav-language a').on('click', function(ev) {
+                   ev.preventDefault();
+                    var $button = $(this);
+                    var currLang = $button.data('ver-lang');
+
+                    $('.nav-language li').removeClass('active');
+                    $button.parent().addClass('active');
+
+                    localStorage.setItem('lang-version', currLang);
+                    singlePage.changeLanguageVersion();
+                });
+            },
+
+            /* ----- show hide - button back to top ----- */
+            btnShowHide: function() {
+                var $pageTop = $(window).scrollTop();
+                var $bannerHeight = $('.site-banner').height();
+                return ( $pageTop > $bannerHeight / 3 ) ? singlePage.$btnBackToTop.fadeIn() : singlePage.$btnBackToTop.fadeOut()
+            },
+
+            /* ----- button back to top ----- */
+            btnBackToTop: function() {
+                singlePage.$btnBackToTop.on('click', function(ev) {
+                    ev.preventDefault();
+                    ev.stopPropagation();
+
+                    $('body').animate({
+                       scrollTop: 0
+                   }, 800);
+                });
+            }
+        };
+
+        singlePage.init();
+    });
+
+})();
