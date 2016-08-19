6904722e1bbbab526020564aee8a458d6ab927d8
Marcin Bogucki <mar.bogucki@gmail.com>
2016-08-19 10:24:18 +0200

    add lang version en

--- js/lang-version.js
+++ js/lang-version.js
@@ -9,7 +9,24 @@ var lang = {
         skills: 'Umiejętności',
         contact: 'Kontakt',
 
-        button_cv: 'Pobierz moje CV'
+        banner_text_bold: 'Szukasz nowoczesnych i kreatywnych rozwiązań?',
+        banner_text_normal: 'Razem zrealizujemy Twój cel.',
+
+        about_01: 'Nazywam się Marcin i jestem Web Designerem. W 2008 roku ukończyłem studia na Uniwersytecie Warmińsko-Mazurskim w Olsztynie, na kierunku Edukacja Techniczno-Informatyczna. Od 6 lat zajmuję się projektowaniem stron i aplikacji internetowych, a od niedawna realizuję projekty także jako front-end developer.',
+        about_02: 'Swoją uwagę koncentruję na rozwijaniu umiejętności w tworzeniu responsywnych stron i aplikacji internetowych z użyciem HTML, CSS i JavaScript oraz na poznawaniu najnowocześniejszych technologii. Powierzone projekty traktuję odpowiedzialnie i z zaangażowaniem. Moje profesjonalne podejście pomaga sprostać największym oczekiwaniom.',
+        about_03: 'Lubię pracować w zespołach, w których ludzie wymieniają się rozwiązaniami i pomysłami. Dzięki temu nabieram większego doświadczenia, a zespół pracuje szybciej i efektywniej. To właśnie zgranie zespołu powoduje, że jest on w stanie zrealizować nawet najtrudniejsze zagadnienia informatyczne.',
+
+        work_01: 'Nadzór i przygotowanie identyfikacji wizualnej firmy. Utworzenie logotypu, papieru firmowego, ulotek, teczek, strony internetowej. Zaprojektowanie interfejzu użytkownika dla szafy medycznej z lekami - "Monalisa", oraz interfejsów medycznych rozwiązań dla sal operacyjnych "Vitruvius". Przygotowanie projektu graficznego dla interfejsu graficznego programu "Leonardo" - do obsługi oddziału szpitalnego i zarządzania pacjentami.',
+        work_02: 'Współpraca z programistami PHP w zakresie aplikacji internetowych z wykorzystaniem języka HTML i CSS. Praca nad projektami dla marek i kampanii reklamowych: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
+        work_03: 'Przygotowywanie i nadzór nad projektami interfejsów komunikacji audio-wizualnej między użytkownikami rozproszonymi na całym świecie. System wspaniale sprawdził się także jako narzędzie e-learningowe do prowadzenia szkoleń on-line. Opróćz możliwości rozmowy audio-wideo, system zapewniał prowadzenie chat-ów, przesyłanie plików, prowadzenie głosowań tajnych oraz jawnych,  tworzenie ankiet oraz testów.',
+        work_04: 'Wytwarzanie oprogramowania dzinników elektronicznych "Nasze Oceny" dla szkół podstawowych, gimnazjalnych i ponadpodstawowych. Przygotowywanie projektów graficznych i nadzór nad rozwojem produktu. Przygotowywanie ikon. Tworzenie szkolnych podręczników typu E-book.',
+        work_05: 'Przygotowywanie kodu HTML 4.0 i CSS stron i sklepów internetowych. Rozwój istniejących produktów dla marek: Microsoft Polska, Fuji Film, italux.pl, Water House.',
+
+        skills_txt: 'W swojej pracy wykorzystuję poniższe narzędzia z zakresu front-end i web designu. Ich łączenie pozwala na realizację najbardziej skomplikowanych projektów i osiągnięcie wszystkich zamierzonych celów.',
+
+        button_cv: 'Pobierz moje CV',
+        cv_heading:"Więcej informacji",
+        cv_paragraph: "Zobacz moje CV, aby dowiedzieć się wszystkich szczegółów o mojej pracy i doświadczeniu."
 
     },
 
@@ -22,7 +39,24 @@ var lang = {
         skills: 'Skills',
         contact: 'Contact',
 
-        button_cv: 'Download my CV'
+        banner_text_bold: 'You\'re looking for modern and creative solutions?',
+        banner_text_normal: 'Together accomplished Your goal.',
+
+        about_01: 'My name is Marcin and I\'m Web Designer. In 2008 I graduate form University of Warmia and Mazury in Olsztyn as MSc Technical-Information Education. For 6 years I\'ve been designing websites and web aplications, and recently I realize projects as well as front-end developer.',
+        about_02: 'I am focusing attention on developing skills in creating responsive websites and web applications using HTML, CSS and JavaScript, and getting to know the most modern technologies. Entrusted projects I consider responsibly and with commitment. My professional approach helps meet the highest expectations.',
+        about_03: 'I\'m keen on working with teams, where people exchanges solutions and ideas. In that, I\'m acquire greater experience, and the temat works faster and efficiently. It just companionate the team means that he is able to accomplish even the most difficult issues of information.',
+
+        work_01: '',
+        work_02: 'Cooperation with PHP programmers in the field of web applications using HTML and CSS. Working on projects for brands and advertising campaigns: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
+        work_03: '',
+        work_04: 'Generate of software electronic registers "Nasze Oceny" for primary, middle and secondary schools. Creating graphic design and intendance product development. Creating graphic icon and educational textbooks like E-book.',
+        work_05: 'Creating HTML4.0 and CSS code for websites and online stores. Development of existing products for brand: Microsoft Polska, Fuji Film, italux.pl, Water House.',
+
+        skills_txt: 'In my work I use the following tools in the field of front-end and web design.  Their joining allows for the implementation of the most complicated projects and achieve all objectives.',
+
+        button_cv: 'Download my CV',
+        cv_heading:"More information",
+        cv_paragraph: "See my CV, and to find out more about my work and experience."
     }
 
 };
