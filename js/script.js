9d8e83f9e5c345874b612c728e97e866a4c4e8a5
Marcin Bogucki <mar.bogucki@gmail.com>
2016-08-19 11:42:21 +0200

    maps

--- js/script.js
+++ js/script.js
@@ -180,8 +180,8 @@
     var map;
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 54322780, lng: 18615471},
-            zoom: 12
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
         });
     }
 
