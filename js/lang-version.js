90d89848469ba72ec7a52e86962428053ac71993
Marcin Bogucki <mar.bogucki@gmail.com>
2016-09-01 13:37:15 +0200

    update english version

--- js/lang-version.js
+++ js/lang-version.js
@@ -39,24 +39,24 @@ var lang = {
         skills: 'Skills',
         contact: 'Contact',
 
-        banner_text_bold: 'You\'re looking for modern and creative solutions?',
-        banner_text_normal: 'Together we will accomplish your goal.',
+        banner_text_bold: 'Are you looking for innovative and creative solutions?',
+        banner_text_normal: 'Together we can accomplish your goal.',
 
-        about_01: 'My name is Marcin and I\'m Web Designer. In 2008 I graduatde from University of Warmia and Mazury in Olsztyn as MSc Technical-Information Education. For 6 years I\'ve been designing websites and web aplications, and recently was also realizing projects as front-end developer.',
-        about_02: 'I\'m focusing my attention on developing skills of creating responsive websites and web applications using HTML, CSS and JavaScript, and getting to know the most modern technologies. I take care of entrusted projects with responsibility and commitment. My professional approach helps meet the highest expectations.',
-        about_03: 'I\'m keen on working with teams, where people exchanges solutions and ideas. Thanks to that, I acquire greater experience, and team works faster and more efficiently. It is because of team cooperation that it is able to accomplish even the most difficult IT tasks.',
+        about_01: 'My name is Marcin and I\'m Web Designer. In 2008 I graduated from University of Warmia and Mazury in Olsztyn with a Master’s degree in Technical-Information Education. For the last 6 years I\'ve been designing websites and web applications. Recently I also started to focus my energy on front-end development projects.',
+        about_02: 'I\'m focusing my attention on developing my skills in creating responsive websites and web applications using HTML, CSS and JavaScript, and getting to know the most modern technologies. I take care of entrusted projects with responsibility and commitment. My professional approach helps me to meet the highest expectations.',
+        about_03: 'I\'m keen on team work, when people exchange solutions and ideas. Thanks to that, I acquire greater experience, and the team works faster and more efficiently. It is because of the team cooperation that it is able to accomplish even the most difficult IT tasks.',
 
-        work_01: 'Supervision and preparation of corporate identity. Creation of a logotype, letterhead paper, leaflet, folders, website. Designing user interface for cabinets with medical drugs - "Monalisa", and interfaces medical solutions for surgery "Vitruvius". Preparation of graphic design the interface of the "Leonardo" - to service hospital ward and patient management.',
-        work_02: 'Working with PHP programmers in the field of web applications using HTML and CSS. Working on projects for brands and advertising campaigns: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
-        work_03: 'Preparation and supervision of audio-video communication interfaces projects for users scattered around the world. System worked out wonderfully as well as e-learning tool for on-line training. In addition to audio-video discussions system provides a chat conversation, files transfer, secret conducts and transparent votes , creating surveys and tests.',
-        work_04: 'Generating of software electronic registers "Nasze Oceny" for primary, middle and secondary schools. Creating graphic design and supervision of product development. Creating graphic icons and educational e-books.',
-        work_05: 'Creating HTML4.0 and CSS code for websites and online stores. Development of existing products for brand: Microsoft Polska, Fuji Film, italux.pl, Water House.',
+        work_01: 'Supervision and preparation of visual identification of the company. Creation of a logotype, letterhead paper, leaflet, folders, website. Designing user interface for a medical cabinet - "Monalisa" and additional medically related interfaces for surgical theatres "Vitruvius". Preparation of graphic design for the interface of the "Leonardo" program to supervise hospital wards and help out with patients’ management.',
+        work_02: 'Working along with PHP programmers in a project focusing on web applications using HTML and CSS. Working on projects for brands and their advertising campaigns: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
+        work_03: 'Preparation and supervision of audio-video communication interface projects for users around the world. System also turned out to be a great solution for e-learning and became a powerful tool for on-line training. In addition to audio-video discussions the system has a built in communicator, files transfer, secret and transparent voting system and creating surveys and tests.',
+        work_04: 'Generating software electronic registers "Nasze Oceny" for primary, secondary and high schools. Creating graphic design and supervision of the product development. Creating graphic icons and educational e-books.',
+        work_05: 'Creating HTML4.0 and CSS code for websites and online stores. Development of existing products for brands such as: Microsoft Polska, Fuji Film, italux.pl, Water House.',
 
-        skills_txt: 'In my work I use the following tools in the field of front-end and web design. Their combination allows to implement the most complicated projects and achieve all objectives.',
+        skills_txt: 'In my work I use the following tools in the field of front-end and web design. Their combination allows me to implement solutions to the most complicated projects and achieve all set objectives.',
 
         button_cv: 'Download my CV',
         cv_heading: 'More information',
-        cv_paragraph: 'See my CV, and to find out more about my work and experience.'
+        cv_paragraph: 'Check out my CV to find out more about my work and experience.'
 
     }
 
