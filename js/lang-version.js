e32e2d81dbfb01cd78a36cf30d3d8a20934f5319
Marcin Bogucki <mar.bogucki@gmail.com>
2016-09-02 09:42:58 +0200

    add today text to language version

--- js/lang-version.js
+++ js/lang-version.js
@@ -16,6 +16,8 @@ var lang = {
         about_02: 'Swoją uwagę koncentruję na rozwijaniu umiejętności w tworzeniu responsywnych stron i aplikacji internetowych z użyciem HTML, CSS i JavaScript oraz na poznawaniu najnowocześniejszych technologii. Powierzone projekty traktuję odpowiedzialnie i z zaangażowaniem. Moje profesjonalne podejście pomaga sprostać największym oczekiwaniom.',
         about_03: 'Lubię pracować w zespołach, w których ludzie wymieniają się rozwiązaniami i pomysłami. Dzięki temu nabieram większego doświadczenia, a zespół pracuje szybciej i efektywniej. To właśnie zgranie zespołu powoduje, że jest on w stanie zrealizować nawet najtrudniejsze zagadnienia informatyczne.',
 
+        now: 'dzisiaj',
+
         work_01: 'Nadzór i przygotowanie identyfikacji wizualnej firmy. Utworzenie logotypu, papieru firmowego, ulotek, teczek, strony internetowej. Zaprojektowanie interfejsu użytkownika dla szafy medycznej z lekami - "Monalisa", oraz interfejsów medycznych rozwiązań dla sal operacyjnych "Vitruvius". Przygotowanie projektu graficznego interfejsu programu "Leonardo" - do obsługi oddziału szpitalnego i zarządzania pacjentami.',
         work_02: 'Współpraca z programistami PHP w zakresie aplikacji internetowych z wykorzystaniem języka HTML i CSS. Praca nad projektami dla marek i kampanii reklamowych: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
         work_03: 'Przygotowywanie i nadzór nad projektami interfejsów komunikacji audio-wizualnej między użytkownikami rozproszonymi na całym świecie. System wspaniale sprawdził się także jako narzędzie e-learningowe do prowadzenia szkoleń on-line. Opróćz możliwości rozmowy audio-wideo, system zapewniał prowadzenie chat-ów, przesyłanie plików, prowadzenie głosowań tajnych oraz jawnych,  tworzenie ankiet oraz testów.',
@@ -45,6 +47,8 @@ var lang = {
         about_01: 'My name is Marcin and I\'m Web Designer. In 2008 I graduated from University of Warmia and Mazury in Olsztyn with a Master’s degree in Technical-Information Education. For the last 6 years I\'ve been designing websites and web applications. Recently I also started to focus my energy on front-end development projects.',
         about_02: 'I\'m focusing my attention on developing my skills in creating responsive websites and web applications using HTML, CSS and JavaScript, and getting to know the most modern technologies. I take care of entrusted projects with responsibility and commitment. My professional approach helps me to meet the highest expectations.',
         about_03: 'I\'m keen on team work, when people exchange solutions and ideas. Thanks to that, I acquire greater experience, and the team works faster and more efficiently. It is because of the team cooperation that it is able to accomplish even the most difficult IT tasks.',
+
+        now: 'today',
 
         work_01: 'Supervision and preparation of visual identification of the company. Creation of a logotype, letterhead paper, leaflet, folders, website. Designing user interface for a medical cabinet - "Monalisa" and additional medically related interfaces for surgical theatres "Vitruvius". Preparation of graphic design for the interface of the "Leonardo" program to supervise hospital wards and help out with patients’ management.',
         work_02: 'Working along with PHP programmers in a project focusing on web applications using HTML and CSS. Working on projects for brands and their advertising campaigns: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
