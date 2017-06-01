e8bcf4ad5d72a6800e2d7aaf52d2b0678566aee4
Marcin Bogucki <mar.bogucki@gmail.com>
2017-06-01 20:45:31 +0200

    update

--- js/lang-version.js
+++ js/lang-version.js
@@ -18,12 +18,12 @@ var lang = {
 
         now: 'dzisiaj',
 
+        work_00: 'Aktualizacja i tworzenie sklepów internetowych. Praca nad projektami dla marek: Reserved, Mohito, Sinsay, Cropp, House.',
         work_01: 'Nadzór i przygotowanie identyfikacji wizualnej firmy. Utworzenie logotypu, papieru firmowego, ulotek, teczek, strony internetowej. Zaprojektowanie interfejsu użytkownika dla szafy medycznej z lekami - "Monalisa", oraz interfejsów medycznych rozwiązań dla sal operacyjnych "Vitruvius". Przygotowanie projektu graficznego interfejsu programu "Leonardo" - do obsługi oddziału szpitalnego i zarządzania pacjentami.',
         work_02: 'Współpraca z programistami PHP w zakresie aplikacji internetowych z wykorzystaniem języka HTML i CSS. Praca nad projektami dla marek i kampanii reklamowych: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
         work_03: 'Przygotowywanie i nadzór nad projektami interfejsów komunikacji audio-wizualnej między użytkownikami rozproszonymi na całym świecie. System wspaniale sprawdził się także jako narzędzie e-learningowe do prowadzenia szkoleń on-line. Opróćz możliwości rozmowy audio-wideo, system zapewniał prowadzenie chat-ów, przesyłanie plików, prowadzenie głosowań tajnych oraz jawnych,  tworzenie ankiet oraz testów.',
         work_04: 'Wytwarzanie oprogramowania dzinników elektronicznych "Nasze Oceny" dla szkół podstawowych, gimnazjalnych i ponadpodstawowych. Przygotowywanie projektów graficznych i nadzór nad rozwojem produktu. Przygotowywanie ikon. Tworzenie szkolnych podręczników typu E-book.',
         work_05: 'Przygotowywanie kodu HTML 4.0 i CSS stron i sklepów internetowych. Rozwój istniejących produktów dla marek: Microsoft Polska, Fuji Film, italux.pl, Water House.',
-        work_06: 'Aktualizacja i tworzenie sklepów internetowych. Praca nad projektami dla marek: Reserved, Mohito, Sinsay, Cropp, House.',
 
         skills_txt: 'W swojej pracy wykorzystuję poniższe narzędzia z zakresu front-end i web designu. Ich łączenie pozwala na realizację najbardziej skomplikowanych projektów i osiągnięcie wszystkich zamierzonych celów.',
 
@@ -51,12 +51,12 @@ var lang = {
 
         now: 'today',
 
+        work_00: 'Update and create online stores. Working on projects for brands: Reserved, Mohito, Sinsay, Cropp, House.',
         work_01: 'Supervision and preparation of visual identification of the company. Creation of a logotype, letterhead paper, leaflet, folders, website. Designing user interface for a medical cabinet - "Monalisa" and additional medically related interfaces for surgical theatres "Vitruvius". Preparation of graphic design for the interface of the "Leonardo" program to supervise hospital wards and help out with patients’ management.',
         work_02: 'Working along with PHP programmers in a project focusing on web applications using HTML and CSS. Working on projects for brands and their advertising campaigns: GET IN Bank, e-Health Solutions, NETIA, primeindustrial.pl, dawca.pl, zawodniacy.pl.',
         work_03: 'Preparation and supervision of audio-video communication interface projects for users around the world. System also turned out to be a great solution for e-learning and became a powerful tool for on-line training. In addition to audio-video discussions the system has a built in communicator, files transfer, secret and transparent voting system and creating surveys and tests.',
         work_04: 'Generating software electronic registers "Nasze Oceny" for primary, secondary and high schools. Creating graphic design and supervision of the product development. Creating graphic icons and educational e-books.',
         work_05: 'Creating HTML4.0 and CSS code for websites and online stores. Development of existing products for brands such as: Microsoft Polska, Fuji Film, italux.pl, Water House.',
-        work_06: 'Update and create online stores. Working on projects for brands: Reserved, Mohito, Sinsay, Cropp, House.',
 
         skills_txt: 'In my work I use the following tools in the field of front-end and web design. Their combination allows me to implement solutions to the most complicated projects and achieve all set objectives.',
 
